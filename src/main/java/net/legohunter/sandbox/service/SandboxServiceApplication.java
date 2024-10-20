378f2b95e5f736ebfbbf1ff4658d3da15eeb8346
tvatt <github@vattima.com>
2024-10-20 18:11:23 -0400

    New sandbox service

---
+++ src/main/java/net/legohunter/sandbox/service/SandboxServiceApplication.java
@@ -0,0 +1,16 @@
+package net.legohunter.sandbox.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class
+})
+public class SandboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxServiceApplication.class, args);
+	}
+
+}
